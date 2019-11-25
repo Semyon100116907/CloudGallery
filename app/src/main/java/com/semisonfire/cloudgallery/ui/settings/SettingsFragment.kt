1ef56d3d7e8e9c5f4c7fcf499ad01a9aa821597b
semisonfire <sotinsemen@gmail.com>
2019-11-25 18:05:04 +0300

    Hide float btn and disable refresh layout on settings screen

--- app/src/main/java/com/semisonfire/cloudgallery/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/semisonfire/cloudgallery/ui/settings/SettingsFragment.kt
@@ -1,5 +1,7 @@
 package com.semisonfire.cloudgallery.ui.settings
 
+import android.support.design.widget.FloatingActionButton
+import android.support.v4.widget.SwipeRefreshLayout
 import android.view.View
 import com.semisonfire.cloudgallery.R
 import com.semisonfire.cloudgallery.core.mvp.MvpView
@@ -7,21 +9,14 @@ import com.semisonfire.cloudgallery.core.ui.BaseFragment
 
 interface SettingsView : MvpView
 
-
-enum class AudioStateView {
-  LOADER,
-  CONTENT,
-  EMPTY,
-  ERROR
-}
-
 class SettingsFragment : BaseFragment<SettingsView, SettingsPresenter>() {
 
   override fun bind(view: View) {
     super.bind(view)
-
-//    floatButton.hide()
-//    swipeRefreshLayout.isEnabled = false
+    activity?.let {
+      it.findViewById<FloatingActionButton>(R.id.btn_add_new).hide()
+      it.findViewById<SwipeRefreshLayout>(R.id.swipe_refresh).isEnabled = false
+    }
   }
 
   override fun layout(): Int {
